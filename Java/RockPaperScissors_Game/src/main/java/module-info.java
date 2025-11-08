110beec055296c0eb2fbb9d341c6c182553eae52
D. Rodokhlebov <rodohlebov.dima@gmail.com>
2025-11-08 13:42:11 +0700

    Remove files

--- Java/RockPaperScissors_Game/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.rockpaperscissors_game {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.rockpaperscissors_game to javafx.fxml;
-    exports com.example.rockpaperscissors_game;
-}
